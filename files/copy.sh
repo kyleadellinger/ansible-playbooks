3f3e20e9ac48f27dc0f757fc1046c3854a58309e
kyleadellinger <116576528+kyleadellinger@users.noreply.github.com>
2023-09-13 18:35:53 -0400

    Update copy.sh

--- files/copy.sh
+++ files/copy.sh
@@ -4,4 +4,4 @@ cp /home/ubuntu/pass/appenv.sh ~ && sudo chmod +x appenv.sh;
 cp /home/ubuntu/pass/shell/curlCLI.sh ~ && sudo chmod +x curlCLI.sh;
 cp /home/ubuntu/pass/shell/curlUA.sh ~ && sudo chmod +x curlUA.sh;
 sudo chmod +x /home/ubuntu/mend;
-echo "PATH='/home/ubuntu/mend:$PATH'" >> ~/.bashrc && source .bashrc;
+sudo ln -s /home/ubuntu/mend /usr/local/bin/mend;
